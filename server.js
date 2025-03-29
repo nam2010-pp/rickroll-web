4492212c0187813103b2bb8547b8b5e8d827bea3
nam2010-pp <nam348@gmail.com>
2025-03-29 23:52:30 +0700

    Sửa lỗi phục vụ ads.txt trên Render

--- server.js
+++ server.js
@@ -4,11 +4,16 @@ const path = require("path");
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+app.get("/ads.txt", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html", "ads.txt"));
 });
 
 const PORT = 3000;
+// Phục vụ file ads.txt từ thư mục gốc
+app.get('/ads.txt', (req, res) => {
+    res.sendFile(path.join(__dirname, 'ads.txt'));
+});
 app.listen(PORT, () => {
   console.log(`Server chạy tại: http://localhost:${PORT}`);
+
 });
